62ac3847c994d63285811dfc1c86b5fe53d8f48d
KavishShetty <103624511+KavishShetty@users.noreply.github.com>
2022-05-08 17:45:15 +0530

    values
    values function

--- main.go
+++ main.go
@@ -12,3 +12,15 @@ func main() {
 
 	fmt.Println("!... Hello World ...!")
 }
+
+func values() {
+
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
