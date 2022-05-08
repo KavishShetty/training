8d40bc02b1c3b96eb3b74a9068f20381eca21620
KavishShetty <103624511+KavishShetty@users.noreply.github.com>
2022-05-08 18:08:09 +0530

    test

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 func test() {
 
 	fmt.Println("!... Hello World ...!")
-	fmt.Println("go" + "lang")
+	fmt.Println("go" + "lang" + "Challenge")
 
 	fmt.Println("1+1 =", 1+1)
 	fmt.Println("7.0/3.0 =", 7.0/3.0)
